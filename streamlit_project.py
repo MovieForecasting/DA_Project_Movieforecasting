fa7299aa4010f753fe080db432801940e786bab6
MovieForecasting <movieforecastingdatascientest@gmail.com>
2025-03-05 14:20:42 +0100

    Update streamlit_project.py

--- streamlit_project.py
+++ streamlit_project.py
@@ -15,13 +15,19 @@ from sklearn.metrics import r2_score
 st.title("Prévision du succès d'un film")
 
 st.sidebar.title("Sommaire")
-pages=["Présentation du projet", "Exploration du Dataset et DataViz'", "Pré-processing","Modélisation","Machine Learning","Conclusion"]
+
+pages=["Présentation du projet", "Exploration du Dataset et DataViz'", "Préprocessing", "Modélisation", "Machine Learning", "Conclusion"]
+
 page=st.sidebar.radio("Aller vers", pages)
 
 if page == pages[0]:
+  
   st.write("### Présentation du projet")
+  
   st.write("Ce projet a été réalisé dans le cadre de notre formation en Data Analyse via l'organisme Data Scientest.")
   st.write("L'objectif de ce projet est de prédire le succès d'un film au box-office en utilisant le jeu de donnée issu de TMDB : 'The Movie DataBase' où nous pouvons obtenir des informations comme le budget, les recettes, le genre, les acteurs et les réalisateurs.")
   st.write("Grâce à l’analyse de ces données, nous pouvons mieux comprendre lequelles ont une influence dans le succès d'un film.")
+  
   st.write("### Problématique")
+  
   st.write("Quels sont les éléments clés qui influencent le succès d’un film ? Peut-on prédire ce succès à partir des données disponibles ?")
