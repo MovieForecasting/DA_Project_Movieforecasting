970b5f3d2edcd3fb98b3a19a95c70122c6fcb5fe
MovieForecasting <movieforecastingdatascientest@gmail.com>
2025-03-05 13:25:03 +0100

    Update streamlit_project.py

--- streamlit_project.py
+++ streamlit_project.py
@@ -1,1 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+st.title("Prévision du succès d'un film")
+
+st.sidebar.title("Sommaire")
+pages=["Présentation du projet", "Exploration du Dataset et DataViz'", "Pré-processing","Modélisation","Machine Learning","Conclusion"]
+page=st.sidebar.radio("Aller vers", pages)
